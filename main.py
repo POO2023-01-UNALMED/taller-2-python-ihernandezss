71a72817a0d39ba2c77f11641b96e8a989b87f5a
ihernandezss <118498504+ihernandezss@users.noreply.github.com>
2023-02-20 18:15:32 -0500

    Taller 2

---
+++ main.py
@@ -0,0 +1,53 @@
+class Auto:
+    def __init__(self, modelo, precio, asientos, marca, motor, registro):
+    self.modelo=modelo
+    self.precio=precio
+    self.asientos=asientos
+    self.marca=marca
+    self.motor=motor
+    self.registro=registro
+    
+    //metodos
+    def cantidadAsientos(self):
+        n=0
+        for i in self.asientos:
+            if i!=None:
+                n+=1
+        return n
+
+    def verificarIntegridad(self):
+        for i in self.asientos:
+            if i!=None:
+                if i.registro==self.registro and i.registro==self.motor.registro:
+                    return ('Auto original')
+        return ("Las piezas no son originales")
+
+    
+
+
+
+
+class Asiento:
+    def __init__(self, color, precio, registro):
+    self.color=color
+    self.precio=precio
+    self.registro=registro
+
+    def cambiarColor(self,color):
+    colores=["rojo", "amarillo", "verde", "blanco",  "negro"]
+    if color in colores:
+        self.color=color
+
+
+class Motor:
+    def __init__(self, numero, tipo, registro):
+    self.numero=numero
+    self.tipo=tipo
+    self.registro=registro
+
+    def asignarTipo(self,tipo):
+        if tipo=='gasolina' or tipo=='electrico':
+            self.tipo=tipo
+
+    def cambiarRegistro(self,registro):
+        self.registro=registro
